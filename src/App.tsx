4ab2c54f9c0a70e9eba979746cc06a6fdd836a3d
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2025-03-03 18:58:29 +0530

    Shadnc init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/ui/button"
+
 const App = () => {
   return (
     <div className="min-h-screen flex items-center justify-center text-red-500">
       <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <Button
+        variant={"outline"}
+        className="mt-4"
+      >Click me</Button>
     </div>
   )
 }
