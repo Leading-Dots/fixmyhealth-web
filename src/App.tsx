69194519d72b5dc14423ccc36e3ae6b51f5d7b12
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2025-03-03 17:14:35 +0530

    App init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-red-500">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
